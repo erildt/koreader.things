b3087b90c06e9e49944b19986abe2812ca0fcb1b
Jimothy <240087026+titandrive@users.noreply.github.com>
2026-07-09 09:08:53 -0700

    Create 2--ui-font_updated.lua

---
+++ patches/2--ui-font_updated.lua
@@ -0,0 +1,111 @@
+-- This is a modified version of sebdelsol's ui-font patch that fixes a few issues. https://github.com/sebdelsol/KOReader.patches
+-- NAME IT "2--ui-font.lua": it NEEDS to be the 1st user patch to be executed
+
+local Font = require("ui/font")
+local Version = require("version")
+local _ = require("gettext")
+local T = require("ffi/util").template
+local UIManager = require("ui/uimanager")
+local cre = require("document/credocument"):engineInit()
+local logger = require("logger")
+
+if Version:getNormalizedCurrentVersion() < Version:getNormalizedVersion("v2025.04-115") then
+    local orig_string_rep = string.rep -- fix https://github.com/koreader/koreader/issues/13925
+    getmetatable("").__index.rep = function(self, n)
+        if n < math.huge then return orig_string_rep(self, n) end
+        return self
+    end
+end
+
+-- UI font
+local UIFont = {
+    setting = { name = "ui_font_name", default = "Noto Sans" },
+    font_type = { regular = "NotoSans-Regular.ttf", bold = "NotoSans-Bold.ttf" },
+}
+
+function UIFont:getSetting() return G_reader_settings:readSetting(self.setting.name, self.setting.default) end
+function UIFont:setSetting(value) G_reader_settings:saveSetting(self.setting.name, value) end
+
+function UIFont:init()
+    self.font_list = {}
+    self.fonts = {}
+    for _, name in ipairs(cre.getFontFaces()) do
+        local path_regular = cre.getFontFaceFilenameAndFaceIndex(name)
+        if path_regular then
+            -- ask crengine for the real bold face instead of guessing the filename,
+            -- since FontList's own directory scan can disagree with cre's paths on some devices (e.g. Boox)
+            local path_bold = cre.getFontFaceFilenameAndFaceIndex(name, true) or path_regular
+            table.insert(self.font_list, name)
+            self.fonts[name] = { regular = path_regular, bold = path_bold }
+        end
+    end
+
+    local type_font = {}
+    self.to_be_replaced = {}
+    -- stylua: ignore start
+    for typ, font in pairs(self.font_type) do type_font[font] = typ end
+    for name, font in pairs(Font.fontmap) do self.to_be_replaced[name] = type_font[font] end
+    -- stylua: ignore end
+
+    self:setFont()
+end
+
+function UIFont:setFont(name)
+    local current_name = self:getSetting()
+    if name ~= current_name then
+        name = name or current_name
+        if not self.fonts[name] then name = DEFAULT end
+        for font, typ in pairs(self.to_be_replaced) do
+            Font.fontmap[font] = self.fonts[name][typ]
+        end
+        self:setSetting(name)
+        return true
+    end
+end
+
+function UIFont:menu()
+    return {
+        text_func = function() return T(_("UI font: %1"), self:getSetting()) end,
+        sub_item_table_func = function()
+            local items = {}
+            for i, name in ipairs(self.font_list) do
+                table.insert(items, {
+                    text = name,
+                    enabled_func = function() return name ~= self:getSetting() end,
+                    font_func = function(size) return Font:getFace(self.fonts[name].regular, size) end,
+                    callback = function()
+                        if self:setFont(name) then
+                            UIManager:askForRestart(_("Restart to apply the UI font change"))
+                        end
+                    end,
+                })
+            end
+            return items
+        end,
+    }
+end
+
+--singleton
+UIFont:init()
+
+-- menu
+local FileManagerMenu = require("apps/filemanager/filemanagermenu")
+local ReaderMenu = require("apps/reader/modules/readermenu")
+
+local function patch(menu, order)
+    table.insert(order.setting, "----------------------------")
+    table.insert(order.setting, "ui_font")
+    menu.menu_items.ui_font = UIFont:menu()
+end
+
+local orig_FileManagerMenu_setUpdateItemTable = FileManagerMenu.setUpdateItemTable
+function FileManagerMenu:setUpdateItemTable()
+    patch(self, require("ui/elements/filemanager_menu_order"))
+    orig_FileManagerMenu_setUpdateItemTable(self)
+end
+
+local orig_ReaderMenu_setUpdateItemTable = ReaderMenu.setUpdateItemTable
+function ReaderMenu:setUpdateItemTable()
+    patch(self, require("ui/elements/reader_menu_order"))
+    orig_ReaderMenu_setUpdateItemTable(self)
+end
